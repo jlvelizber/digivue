92284987065d6c4d1809a94339f0a1b7f18c8fc0
Jorge Veliz <jorgeconsalvacion@gmail.com>
2023-12-20 20:32:03 -0500

    refactor: Ajusta la imagen del digimon centrada en mobile

--- resources/js/Components/DigimonListItem.vue
+++ resources/js/Components/DigimonListItem.vue
@@ -9,7 +9,7 @@ defineProps<{
 <template>
     <Link :href="route('digimon.show', digimon.id)">
     <div class="bg-gray-100 hover:bg-gray-200 p-2">
-        <img v-bind:src="digimon.image" class="object-cover" />
+        <img v-bind:src="digimon.image" class="object-cover mx-auto" />
         <p class="text-center text-gray-500 hover:text-gray-700"> #{{ digimon.id }}</p>
         <p class="text-center text-blue-500 hover:text-blue-700 font-bold text-sm"> {{ digimon.name }}</p>
     </div>
